2f3a06cc8a3e8048a643e9e96c109e8ad5f0076a
ujas-m-simformsolutions <ujas.m@simformsolutions.com>
2023-03-10 15:06:47 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,92 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+BuildContext? buildContext;
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void _incrementCounter() {
+    final entry = OverlayEntry(
+      builder: (_) => Stack(
+        children: [
+          Positioned(
+            top: 200,
+            left: 100,
+            child: IgnorePointer(
+              ignoring: false,
+              child: GestureDetector(
+                onTap: () {
+                  print('object2');
+                },
+                // behavior: HitTestBehavior.translucent,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    Overlay.of(buildContext!).insert(
+      entry,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Theme.of(context);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        //visualDensity: const VisualDensity(vertical: 1)
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('widget.title'),
+        ),
+        body: Stack(
+          children: [
+            Builder(builder: (context) {
+                buildContext = context;
+              return GestureDetector(
+                onTap: () {
+                  print('object');
+                },
+                child: Container(
+                  color: Colors.yellow,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        'You have pushed the button this many times:',
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
@@ -33,8 +105,41 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  OverlayEntry? entry;
 
   void _incrementCounter() {
+    entry = OverlayEntry(
+      builder: (_) => Stack(
+        children: [
+          Positioned(
+            top: 200,
+            left: 100,
+            child: IgnorePointer(
+              ignoring: false,
+              child: GestureDetector(
+                onTap: () {
+                  print('object2');
+                },
+                // behavior: HitTestBehavior.translucent,
+                child: GestureDetector(
+                  onTap: () {
+                    print('object3');
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    Overlay.of(buildContext!).insert(
+      entry!,
+    );
     setState(() {
       _counter++;
     });
@@ -42,24 +147,38 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    buildContext = context;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-
-          ],
-        ),
+      body: Stack(
+        children: [
+          Builder(builder: (context) {
+            //  buildContext = context;
+            return GestureDetector(
+              onTap: () {
+                print('object');
+              },
+              child: Container(
+                color: Colors.yellow,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'You have pushed the button this many times:',
+                    ),
+                    Text(
+                      '$_counter',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+          Testing(),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -67,5 +186,65 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+}
+
+class Testing extends StatefulWidget {
+  const Testing({Key? key}) : super(key: key);
+
+  @override
+  State<Testing> createState() => _TestingState();
+}
+
+class _TestingState extends State<Testing> {
+  OverlayEntry? entry;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 2),
+      _incrementCounter,
+    );
+  }
+
+  void _incrementCounter() {
+    entry = OverlayEntry(
+      builder: (_) => Stack(
+        children: [
+          Positioned(
+            top: 200,
+            left: 100,
+            child: IgnorePointer(
+              ignoring: false,
+              child: GestureDetector(
+                onTap: () {
+                  print('object2');
+                },
+                // behavior: HitTestBehavior.translucent,
+                child: GestureDetector(
+                  onTap: () {
+                    print('object3');
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    Overlay.of(context).insert(
+      entry!,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
